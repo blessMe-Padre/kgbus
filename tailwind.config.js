9828575e7495c2d778ce4df60e4bed1da97f6d2a
Сергей Субботин <zarodiny@yandex.ru>
2024-05-20 09:52:38 +1000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    screens: {
+      'sm': '768px',
+      'md': '1024px',
+      'lg': '1280px',
+      'xl': '1440px',
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#292e35',
+      'green': '#6cb778',
+      'white': '#fff',
+      'gray': '#b5c2d7',
+      'light-gray': '#ecf3f7'
+    },
+
+    container: {
+      padding: '20px',
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        'bodyFont': ['archangelsk', 'regular'],
+      }
+    }
+  },
+  plugins: [],
+}
+

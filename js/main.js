d019a30e835288923c91d70f445a9d29333130f4
Medved <medvedosin@gmail.com>
2024-05-22 16:45:09 +1000

    Страница "Об учреждении"
    Оптимизация кода

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ import { initScrollToTop } from "./module/scroll-to-top.js";
 import { initScroll } from "./module/scroll.js";
 import { initAccordions } from "./module/accordions.js";
 import { initArrowClass } from "./module/arrow-class-menu.js";
+import { initAnimateClass } from "./module/add_class_for_animation.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('подключен скрипт main.js');
 
-    initNav();
+    //initNav();
     initAccordions();
     initArrowClass();
     // initSlider();
@@ -17,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
     // initScrollToTop();
     // initScroll();
     // baguetteBox.run('.gallery-wrapper');
+
+    initAnimateClass();
 
     // DOMContentLoaded..
 

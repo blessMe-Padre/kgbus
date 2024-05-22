44cbbfee01aad4200466030155b56d4f4aaf0731
Сергей Субботин <zarodiny@yandex.ru>
2024-05-22 16:43:00 +1000

    feat: добавляет стили

--- js/module/appendElement.js
+++ js/module/appendElement.js
@@ -2,5 +2,8 @@ export const initAppend = () => {
     const headerContact = document.querySelector('.header-contact');
     const headerGroup = document.querySelector('.header-group');
 
-    headerGroup.append(headerContact);
+    // при ширине экрана < 769px 
+    if (window.innerWidth < 769) {
+        headerGroup.append(headerContact);
+    }
 }

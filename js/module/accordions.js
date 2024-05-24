68f6da9372c8dfc3ce14ddcf28193b4286cfa973
Сергей Субботин <zarodiny@yandex.ru>
2024-05-24 16:57:14 +1000

    feat: добавляет стили

--- js/module/accordions.js
+++ js/module/accordions.js
@@ -27,18 +27,28 @@ export const initAccordions = () => {
     // Это не для меню
     const openContainer = document.querySelectorAll('.open_container');
 
-    openContainer.forEach(function(container) {
+    openContainer.forEach(function (container) {
         const list = container.querySelectorAll('li');
 
-        list.forEach(function(li){
+        list.forEach(function (li) {
             const button = li.querySelector('.title');
             const content = li.querySelector('.content');
 
-            button.addEventListener('click', function(object) {
-                if(content) {
+            button.addEventListener('click', (evt) => {
+                if (content) {
+                    const currentButton = evt.currentTarget;
+                    currentButton.classList.toggle('is-active');
                     content.classList.toggle('is-active');
+
+                    if (currentButton.classList.contains('is-active')) {
+                        content.style.maxHeight = 'max-content';
+                        // content.style.maxHeight = content.scrollHeight + 'px';
+                    } else {
+                        content.style.maxHeight = null;
+                    }
                 }
             });
+
         });
     });
 }
--- js/module/nav.js
+++ js/module/nav.js
@@ -8,22 +8,30 @@ export const initNav = () => {
     const menuButtonMobile = document.querySelector('.btn-close-menu-mobile');
     const servicesMenuMobile = document.querySelector('.services-menu--mobile');
 
-    menuButton.addEventListener('click', e => {
-        menuButton.classList.toggle('active');
-        servicesMenu.classList.toggle('is-active');
-        // body.classList.toggle('lock');
-    });
-    menuButtonMobile.addEventListener('click', e => {
-        menuButtonMobile.classList.toggle('active');
-        servicesMenuMobile.classList.toggle('is-active');
-        // body.classList.toggle('lock');
-    });
-    mobileMenuLinks.forEach(link => {
-        link.addEventListener('click', (evt) => {
-            evt.preventDefault();
-            menuButton.classList.remove('active');
-            mobileMenu.classList.remove('is-active');
-            // body.classList.remove('lock');
+    if (menuButton) {
+        menuButton.addEventListener('click', e => {
+            menuButton.classList.toggle('active');
+            servicesMenu.classList.toggle('is-active');
+            // body.classList.toggle('lock');
         });
-    });
+    }
+
+    if (menuButtonMobile) {
+        menuButtonMobile.addEventListener('click', e => {
+            menuButtonMobile.classList.toggle('active');
+            servicesMenuMobile.classList.toggle('is-active');
+            // body.classList.toggle('lock');
+        });
+    }
+
+    if (mobileMenuLinks) {
+        mobileMenuLinks.forEach(link => {
+            link.addEventListener('click', (evt) => {
+                evt.preventDefault();
+                menuButton.classList.remove('active');
+                mobileMenu.classList.remove('is-active');
+                // body.classList.remove('lock');
+            });
+        });
+    }
 }

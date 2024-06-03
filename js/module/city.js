20fd7145ef653a68801a58410373a9d48614acf0
Сергей Субботин <zarodiny@yandex.ru>
2024-06-03 14:32:26 +1000

    feat: добавляет выбор города

--- js/module/city.js
+++ js/module/city.js
@@ -1,13 +1,15 @@
 export const initCityChooser = () => {
 
+    // TO DO: поменять url адреса на корректные
+    const NAHODKA_URL = 'http://127.0.0.1:5500/404.html';
+    const USSURISK_URL = 'http://127.0.0.1:5500/products.html';
+    const VLADIVOSTOK_URL = 'http://127.0.0.1:5500/about.html';
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-        let userLocation = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        };
-        // console.log(userLocation); // Выводит локацию в консоль
-    });
+    const select = document.querySelector('.city-select');
+    const cityPopup = document.querySelector('.city-popup');
+    const span = document.querySelector('.city-popup span');
+    const yesBtn = document.querySelector('.city-popup-yes');
+    const noBtn = document.querySelector('.city-popup-no');
 
     const citiesCoordinates = {
         'Владивосток': { latitude: 43.1155, longitude: 131.8855 },
@@ -15,49 +17,80 @@ export const initCityChooser = () => {
         'Уссурийск': { latitude: 43.8029, longitude: 131.9458 }
     };
 
-    navigator.geolocation.getCurrentPosition(function (position) {
-        let userLocation = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        };
-        console.log(userLocation); // Выводит локацию в консоль
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let userLocation = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            };
 
-        let closestCity = null;
-        let smallestDifference = Infinity;
+            let closestCity = 'Уссурийск';
+            let smallestDifference = Infinity;
 
-        for (let city in citiesCoordinates) {
-            let cityLatitude = citiesCoordinates[city].latitude;
-            let difference = Math.abs(userLocation.latitude - cityLatitude);
+            for (let city in citiesCoordinates) {
+                let cityLatitude = citiesCoordinates[city].latitude;
+                let difference = Math.abs(userLocation.latitude - cityLatitude);
 
-            if (difference < smallestDifference) {
-                smallestDifference = difference;
-                closestCity = city;
+                if (difference < smallestDifference) {
+                    smallestDifference = difference;
+                    closestCity = city;
+                }
             }
-        }
+            // получаем из хранилища значение города
+            let savedCity = localStorage.getItem('closestCity');
+            if (!savedCity) {
+                cityPopup.classList.add('is-active');
+            }
 
-        if (closestCity) {
-            console.log(`Ближайший город по широте: ${closestCity}`);
-        }
-    });
+            if (closestCity) {
+                console.log(`Ближайший город по широте: ${closestCity}`);
 
-    const NAHODKA_URL = 'http://127.0.0.1:5500/404.html'; //3
-    const USSURISK_URL = 'http://127.0.0.1:5500/404.html'; //1
-    const VLADIVOSTOK_URL = 'http://127.0.0.1:5500/404.html'; //2
-    const select = document.querySelector('.city-select');
+                localStorage.setItem('closestCity', closestCity);
+                span.innerText = closestCity;
+            }
 
-    select.addEventListener('change', (e) => {
+            // Слушатель нажатия на кнопку yesBtn и закрытие попапа
+            yesBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                let currentCity = localStorage.getItem('closestCity');
 
-        if (e.target.value == 3) {
-            // console.log('Выбран город НАХОДКА');
-            window.location.href = NAHODKA_URL;
-        }
-        if (e.target.value == 1) {
-            // console.log('Выбран город УССУРИЙСК');
-            // window.location.href = NAHODKA_URL;
-        }
-        if (e.target.value == 2) {
-            // console.log('Выбран город ВЛАДИВОСТОК');
-            // window.location.href = NAHODKA_URL;
-        }
-    });
+                if (currentCity == closestCity) {
+                    select.value = closestCity;
+                    window.location.href = 'http://127.0.0.1:5500/404.html';
+                }
+
+                cityPopup.classList.remove('is-active');
+            });
+            // Слушатель нажатия на кнопку noBtn и закрытие попапа
+            noBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                cityPopup.classList.remove('is-active');
+            });
+        });
+    }
+
+    if (select) {
+        select.addEventListener('change', (e) => {
+
+            if (e.target.value == "Находка") {
+                select.value = "Находка";
+                localStorage.setItem('closestCity', "Находка");
+                // window.location.href = NAHODKA_URL;
+            }
+            if (e.target.value == 'Уссурийск') {
+                select.value = "Уссурийск";
+                localStorage.setItem('closestCity', "Уссурийск");
+                // window.location.href = USSURISK_URL;
+            }
+            if (e.target.value == 'Владивосток') {
+                localStorage.setItem('closestCity', "Владивосток");
+                select.value = "Владивосток";
+                // window.location.href = VLADIVOSTOK_URL;
+            }
+        });
+
+
+    }
+
+    // export const
 }
